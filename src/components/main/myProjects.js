de57083ba9b6eb64215c596f5c7d688f7e64f3f5
AhmedAbdalazeem49 <99069962+AhmedAbdalazeem49@users.noreply.github.com>
2025-01-06 15:20:44 +0200

    enhance the view of images in the main page

--- src/components/main/myProjects.js
+++ src/components/main/myProjects.js
@@ -1,3 +1,11 @@
+import Aqdi from "/src/images/aqdi.jpg";
+import Hurmozom from "/src/images/hurmozom.jpg";
+import Kids from "/src/images/kids.jpg";
+import Nabihi from "/src/images/nabihi.jpg";
+import Orasya from "/src/images/oraysa.jpg";
+import QickTech from "/src/images/qickteck.jpg";
+import Relish from "/src/images/relish.jpg";
+import Renzo from "/src/images/renzo.jpg";
 import Saar from "/src/images/saar.jpg";
 export const myProjects = [
   {
@@ -11,7 +19,7 @@ export const myProjects = [
   {
     projectTitle: "Qicktech",
     category: "all",
-    imgPath: "/src/images/qickteck.jpg",
+    imgPath: QickTech,
     projectDescription:
       "A programming company website with a sleek and intuitive UI, offering information about the company and its services, aimed at developers and clients seeking innovative software solutions.",
     liveDemo: "https://qicktech.vercel.app/",
@@ -19,7 +27,7 @@ export const myProjects = [
   {
     projectTitle: "Orasya",
     category: "all",
-    imgPath: "/src/images/oraysa.jpg",
+    imgPath: Orasya,
     projectDescription:
       "An investment platform that ensures safe transactions, offering users a reliable way to send and receive money while investing in various ventures.",
     liveDemo: "https://orasya.vercel.app/",
@@ -27,7 +35,7 @@ export const myProjects = [
   {
     projectTitle: "Hurmozom",
     category: "all",
-    imgPath: "/src/images/hurmozom.jpg",
+    imgPath: Hurmozom,
     projectDescription:
       "A platform to connect users with skilled workers for household tasks, featuring a user-friendly UI that makes booking services easy and efficient.",
     liveDemo: "https://hurmozom.com/",
@@ -35,7 +43,7 @@ export const myProjects = [
   {
     projectTitle: "Nabihiboot",
     category: "all",
-    imgPath: "/src/images/nabihi.jpg",
+    imgPath: Nabihi,
     projectDescription:
       "A marketing platform integrating WhatsApp and AI to enhance customer outreach, offering businesses tools to streamline communication and achieve marketing goals.",
     liveDemo: "https://nabihaibot.com/",
@@ -43,7 +51,7 @@ export const myProjects = [
   {
     projectTitle: "Renzocash",
     category: "all",
-    imgPath: "/src/images/renzo.jpg",
+    imgPath: Renzo,
     projectDescription:
       "A modern, stylish website that provides a platform for users to make secure investments and track their financial growth with ease.",
     liveDemo: "https://renzocash.vercel.app/",
@@ -51,7 +59,7 @@ export const myProjects = [
   {
     projectTitle: "Aqdi",
     category: "all",
-    imgPath: "/src/images/aqdi.jpg",
+    imgPath: Aqdi,
     projectDescription:
       "A cutting-edge platform for generating online real estate contracts between buyers and sellers, ensuring safety, efficiency, and enhanced user experience.",
     liveDemo: "https://aqdi.sa/",
@@ -59,7 +67,7 @@ export const myProjects = [
   {
     projectTitle: "Relish Spices",
     category: "all",
-    imgPath: "/src/images/relish.jpg",
+    imgPath: Relish,
     projectDescription:
       "A landing page offering a wide range of information about herb seeds and spices, designed to inform users and provide easy access to product details.",
     liveDemo: "https://relish-spices.vercel.app/",
@@ -67,7 +75,7 @@ export const myProjects = [
   {
     projectTitle: "KidsSmart",
     category: "all",
-    imgPath: "/src/images/kids.jpg",
+    imgPath: Kids,
     projectDescription:
       "An educational platform aimed at teaching children programming through interactive lessons. The platform includes a dashboard for teachers, video uploading features, and several other interactive learning tools to engage young learners.",
     liveDemo: "https://kidssmart.tech/",
